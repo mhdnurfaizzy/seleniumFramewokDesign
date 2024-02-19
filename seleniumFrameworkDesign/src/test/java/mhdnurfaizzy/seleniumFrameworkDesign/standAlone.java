bc27641acdb191b3f574e591552af20a2a28c089
mhdnurfaizzy <mhdnurfaizzy@gmail.com>
2024-02-19 13:52:57 +0700

    e2e script

--- seleniumFrameworkDesign/src/test/java/mhdnurfaizzy/seleniumFrameworkDesign/standAlone.java
+++ seleniumFrameworkDesign/src/test/java/mhdnurfaizzy/seleniumFrameworkDesign/standAlone.java
@@ -4,9 +4,15 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,6 +21,7 @@ public class standAlone {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String productName = "ADIDAS ORIGINAL";
 		WebDriverManager.edgedriver().setup();
 		WebDriver driver = new EdgeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -26,13 +33,52 @@ public class standAlone {
 		driver.findElement(By.id("userPassword")).sendKeys("Testing890-");
 		driver.findElement(By.id("login")).click();
 		
+		//waiting load page
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
+		
 		//landed on home page
 		//get list products on page
 		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
-		
 		WebElement prod = products.stream().filter(product->
-		product.findElement(By.cssSelector("b")).getText().equals("ADIDAS ORIGINAL")).findFirst().orElse(null);
+		product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst().orElse(null);
+		
+		//add product to cart
 		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
+		
+		//go to cart page
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		//ng-animating
+		//wait until animate end
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
+		
+		//verify product on cart
+		List<WebElement> cartProduct = driver.findElements(By.cssSelector(".cartSection h3"));
+		Boolean match = cartProduct.stream().anyMatch(titleProduct->titleProduct.getText().equalsIgnoreCase(productName));
+		Assert.assertTrue(match);
+		
+		//Click btn checkout
+		driver.findElement(By.cssSelector("li[class='totalRow'] button[type='button']")).click();
+		
+		//checkout product
+		Actions a = new Actions(driver);
+		a.sendKeys(driver.findElement(By.cssSelector("input[placeholder='Select Country']")), "india").build().perform();
+		
+		//fill details informastion
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(".ta-results")));
+		driver.findElement(By.cssSelector(".ta-item:nth-of-type(2)")).click();
+		
+		//scroll intoView submit button
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,100)");
+		wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".action__submit")));
+		driver.findElement(By.cssSelector(".action__submit")).click();
+		
+		//getText thankyou page
+		String confirmMassage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+		Assert.assertTrue(confirmMassage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+		driver.quit();
 	
 	}
 
