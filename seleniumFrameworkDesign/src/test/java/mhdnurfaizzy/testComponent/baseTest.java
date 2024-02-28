024cbac45f956d6efc88dccba3a6d2aa6c4cf1a9
mhdnurfaizzy <mhdnurfaizzy@gmail.com>
2024-02-28 14:09:33 +0700

    Set Global Parameters using Maven commands and Update tests at run time

--- seleniumFrameworkDesign/src/test/java/mhdnurfaizzy/testComponent/baseTest.java
+++ seleniumFrameworkDesign/src/test/java/mhdnurfaizzy/testComponent/baseTest.java
@@ -35,7 +35,8 @@ public class baseTest {
 		Properties prop = new Properties();
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\main\\java\\mhdnurfaizzy\\resource\\globalData.properties");
 		prop.load(fis);
-		String browserName = prop.getProperty("browser");
+		String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") :prop.getProperty("browser");
+//		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equalsIgnoreCase("edge")) {
 			WebDriverManager.edgedriver().setup();
